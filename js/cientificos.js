3712811761e07b7637cbbe5e9dc3fb6df1560dec
DavidKMLP <dlewperez@gmail.com>
2025-04-13 11:23:53 +0200

    Arreglado rutas relativas

--- js/cientificos.js
+++ js/cientificos.js
@@ -1,10 +1,11 @@
 const cientificos = [
-    {
-      nombre: "Alan Turing",
-      fechaCreacion: "23 de junio de 1912",
-      fechaUtilidad: "7 de junio de 1954",
-      imagen: "img/alanturing.png",
-      wiki: "https://es.wikipedia.org/wiki/Alan_Turing"
-    }
-  ];
+  {
+    nombre: "Alan Turing",
+    fechaCreacion: "23 de junio de 1912",
+    fechaUtilidad: "7 de junio de 1954",
+    imagen: "img/alanturing.png",
+    wiki: "https://es.wikipedia.org/wiki/Alan_Turing"
+  }
+];
+
   
