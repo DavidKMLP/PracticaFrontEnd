0e06527f72419b3ef6c8b4525d64cdf220f55f6b
DavidKMLP <dlewperez@gmail.com>
2025-04-13 17:28:26 +0200

    Limpieza y creacion de ventanas de entidad, cientifico y productos

--- js/cientificos.js
+++ js/cientificos.js
@@ -5,7 +5,7 @@ const cientificos = [
     fechaUtilidad: "30 de junio de 1974",
     imagen: "img/vannevar.png",
     wiki: "https://es.wikipedia.org/wiki/Vannevar_Bush",
-    entidades: ["IBM"]
+    entidades: ["IBM"],
   },
   {
     nombre: "Tim Berners-Lee",
@@ -13,7 +13,8 @@ const cientificos = [
     fechaUtilidad: "Actualidad",
     imagen: "img/tim.png",
     wiki: "https://es.wikipedia.org/wiki/Tim_Berners-Lee",
-    entidades: ["W3C"]
+    entidades: ["W3C"],
+    intro: "Timothy John Berners-Lee, también conocido como Tim Berners-Lee, es un científico de la computación británico, conocido por ser el inventor de la World Wide Web. Propuso un sistema de gestión de información en marzo de 1989, que más tarde se convirtió en la Web.",
   }
   
 ];
