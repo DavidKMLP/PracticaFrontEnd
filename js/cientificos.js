353da737df944e20311a48aefaad4e7ebe56fbed
DavidKMLP <dlewperez@gmail.com>
2025-04-13 12:53:01 +0200

    Relacion cientifico entidad

--- js/cientificos.js
+++ js/cientificos.js
@@ -4,13 +4,16 @@ const cientificos = [
     fechaCreacion: "11 de marzo de 1890",
     fechaUtilidad: "30 de junio de 1974",
     imagen: "img/vannevar.png",
-    wiki: "https://es.wikipedia.org/wiki/Vannevar_Bush"
+    wiki: "https://es.wikipedia.org/wiki/Vannevar_Bush",
+    entidades: ["IBM"]
   },
   {
     nombre: "Tim Berners-Lee",
     fechaCreacion: "8 de junio de 1955",
     fechaUtilidad: "Actualidad",
     imagen: "img/tim.png",
-    wiki: "https://es.wikipedia.org/wiki/Tim_Berners-Lee"
+    wiki: "https://es.wikipedia.org/wiki/Tim_Berners-Lee",
+    entidades: ["W3C"]
   }
+  
 ];
